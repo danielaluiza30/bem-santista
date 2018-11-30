7946967470bfa52a2f4cd60c01bc49968edbdf45
Daniela Luiza Martins Augusto do Nascimento <danielaluiza@gmail.com>
2018-11-30 01:06:57 +0000

    layout do admin arrumado - matheus

--- app/controllers/sou_instituicao_controller.rb
+++ app/controllers/sou_instituicao_controller.rb
@@ -1,7 +1,19 @@
 class SouInstituicaoController < ApplicationController
-    layout "login"
-    
     def index
         render "admin/index"
+    end
+    
+    def necessidades
+        render "admin/necessidades"
+    end
+    
+    layout :choose_layout
+  
+    def choose_layout
+        if action_name == 'index'
+            return 'login'
+        else
+            return 'application'
+        end
     end
 end
